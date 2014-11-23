9b77cbd67cc257a361a7b2ae584771e53f7cbb2e
Vinay <rmdort@gmail.com>
2014-11-23 17:19:34 +0800

    Fixing tab spacing

--- jquery.combo.select.js
+++ jquery.combo.select.js
@@ -9,17 +9,17 @@
 
 // Expose plugin as an AMD module if AMD loader is present:
 (function (factory) {
-		'use strict';
-		if (typeof define === 'function' && define.amd) {
-				// AMD. Register as an anonymous module.
-				define(['jquery'], factory);
-		} else if (typeof exports === 'object' && typeof require === 'function') {
-				// Browserify
-				factory(require('jquery'));
-		} else {
-				// Browser globals
-				factory(jQuery);
-		}
+	'use strict';
+	if (typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module.
+		define(['jquery'], factory);
+	} else if (typeof exports === 'object' && typeof require === 'function') {
+		// Browserify
+		factory(require('jquery'));
+	} else {
+		// Browser globals
+		factory(jQuery);
+	}
 }(function ( $, undefined ) {
 
 	var pluginName = "comboSelect";
